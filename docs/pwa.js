17fedf14d04225a5004709d7272f25546f5e49f8
PRElias <ppuspfc@gmail.com>
2020-05-07 10:34:25 -0300

    pwa - calculo peso ideal

--- docs/pwa.js
+++ docs/pwa.js
@@ -27,9 +27,22 @@ function addToHomeScreen() {
 
 }
 
+function calculaIMC(){
+    $("#IMC").val(($("#Peso").val() / ($("#Altura").val() * $("#Altura").val())));
+};
+
+function calculaPesoIdeal() {
+    //https://brasilescola.uol.com.br/matematica/peso-ideal.htm
+    //formula de Lorentz
+    var pesoIdeal = 0.0;
+    var altura = $("#Altura").unmask().val();
+    pesoIdeal = altura - 100 - ((altura - 150) / 4);
+    $("#PesoIdeal").val(pesoIdeal);
+};
 
 function calcula() {
-    $("#IMC").val(($("#Peso").val() / ($("#Altura").val() * $("#Altura").val())));
+    calculaIMC();
+    calculaPesoIdeal();
 };
 
 function salvaCache() {
