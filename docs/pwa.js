78fff93cf9cfdb6f178bf691f3aa57695ed1a118
PRElias <ppuspfc@gmail.com>
2020-05-07 08:50:49 -0300

    pwa calculando e salvando dados

--- docs/pwa.js
+++ docs/pwa.js
@@ -32,6 +32,44 @@ function calcula() {
     $("#IMC").val(($("#Peso").val() / ($("#Altura").val() * $("#Altura").val())));
 };
 
+function salvaCache() {
+    var paciente = {
+        Nome: $("#Nome").val(), 
+        Idade: $("#Idade").val(), 
+        Peso: $("#Peso").val(), 
+        Altura: $("#Altura").val()
+    };
+    localStorage.setItem(paciente.Nome, JSON.stringify(paciente));
+    //$('#form').get(0).reset();
+    formReset();
+};
+
+//Utilizada essa função por não ser um form HTML real, já que assim ele apagaria ao submeter
+function formReset(){
+    $(':input','#form')
+    .not(':button, :submit, :reset, :hidden')
+    .val('')
+    .removeAttr('checked')
+    .removeAttr('selected');
+
+    $(':input','#calculados')
+    .not(':button, :submit, :reset, :hidden')
+    .val('')
+    .removeAttr('checked')
+    .removeAttr('selected');
+}
+
+function sincroniza() {
+    debugger
+    for (var i = 0; i < localStorage.length; i++){
+
+        localStorage.getItem(localStorage.key(i));
+
+        localStorage.removeItem(localStorage.key(i));
+    }
+};
+
+
 $(document).ready(function () {
     $("#Altura").mask("0.00");
 });
