829f5c8122c8f62dc29536c16fdcd9fb78420724
PRElias <ppuspfc@gmail.com>
2020-05-07 16:49:19 -0300

    testando fix para o touch não funcionando pra assinar

--- docs/pwa.js
+++ docs/pwa.js
@@ -31,9 +31,8 @@ function limpa() {
     $('form').get(0).reset();
 };
 
-function calculaIMC(){
-    if ($("#IMC").val() === "")
-    {
+function calculaIMC() {
+    if ($("#IMC").val() === "") {
         $("#IMC").val(($("#Peso").val() / ($("#Altura").val() * $("#Altura").val())));
     }
 };
@@ -55,9 +54,9 @@ function calcula() {
 
 function salvaCache() {
     var paciente = {
-        Nome: $("#Nome").val(), 
-        Idade: $("#Idade").val(), 
-        Peso: $("#Peso").val(), 
+        Nome: $("#Nome").val(),
+        Idade: $("#Idade").val(),
+        Peso: $("#Peso").val(),
         Altura: $("#Altura").val(),
         Sexo: $("#Sexo").val()
     };
@@ -77,7 +76,7 @@ function salvaCache() {
 
 function sincroniza() {
     debugger
-    for (var i = 0; i < localStorage.length; i++){
+    for (var i = 0; i < localStorage.length; i++) {
 
         var paciente = localStorage.getItem(localStorage.key(i));
 
@@ -87,18 +86,18 @@ function sincroniza() {
             contentType: "application/json",
             dataType: "json",
             data: paciente
-        }).done(function(){
+        }).done(function () {
             localStorage.removeItem(localStorage.key(i));
-        }).fail(function(){
+        }).fail(function () {
             alert("Erro ao sincronizar. Você está no mesmo WiFi do sistema?");
         });
     }
 };
 
-function downloadAssinatura(){
+function downloadAssinatura() {
     var download = document.getElementById("download");
     var image = document.getElementById("quadro").toDataURL("image/png")
-                .replace("image/png", "image/octet-stream");
+        .replace("image/png", "image/octet-stream");
     download.setAttribute("href", image);
 }
 
@@ -106,19 +105,19 @@ function downloadAssinatura(){
 $(document).ready(function () {
     $("#Altura").mask("0.00");
 
-    $("form").submit(function(event){
+    $("form").submit(function (event) {
         event.preventDefault()
     });
 
     var largura = 500;
     var altura = 300;
-    
+
     var quadro = document.getElementById("quadro");
     quadro.setAttribute("width", largura);
     quadro.setAttribute("height", altura);
-    
+
     var ctx = quadro.getContext("2d");
-    
+
     var desenhando = false;
 
     quadro.onmousedown = function (evt) {
@@ -127,7 +126,7 @@ $(document).ready(function () {
     }
 
     quadro.onmouseup = function () {
-        desenhando = false;                
+        desenhando = false;
     }
 
     quadro.onmousemove = function (evt) {
@@ -137,3 +136,114 @@ $(document).ready(function () {
         }
     }
 });
+
+function startup() {
+    var el = document.getElementById("canvas");
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+    el.addEventListener("touchcancel", handleCancel, false);
+    el.addEventListener("touchmove", handleMove, false);
+}
+
+document.addEventListener("DOMContentLoaded", startup);
+
+var ongoingTouches = [];
+
+function handleStart(evt) {
+    evt.preventDefault();
+    var el = document.getElementById("quadro");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        ongoingTouches.push(copyTouch(touches[i]));
+        var color = colorForTouch(touches[i]);
+        ctx.beginPath();
+        ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
+        ctx.fillStyle = color;
+        ctx.fill();
+    }
+}
+
+function handleMove(evt) {
+    evt.preventDefault();
+    var el = document.getElementById("quadro");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var color = colorForTouch(touches[i]);
+        var idx = ongoingTouchIndexById(touches[i].identifier);
+
+        if (idx >= 0) {
+            ctx.beginPath();
+            ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+            ctx.lineTo(touches[i].pageX, touches[i].pageY);
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = color;
+            ctx.stroke();
+
+            ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+        } else {
+        }
+    }
+}
+
+function handleEnd(evt) {
+    evt.preventDefault();
+    var el = document.getElementById("quadro");
+    var ctx = el.getContext("2d");
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var color = colorForTouch(touches[i]);
+        var idx = ongoingTouchIndexById(touches[i].identifier);
+
+        if (idx >= 0) {
+            ctx.lineWidth = 4;
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+            ctx.lineTo(touches[i].pageX, touches[i].pageY);
+            ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
+            ongoingTouches.splice(idx, 1);  // remove it; we're done
+        } else {
+        }
+    }
+}
+
+function handleCancel(evt) {
+    evt.preventDefault();
+    var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        var idx = ongoingTouchIndexById(touches[i].identifier);
+        ongoingTouches.splice(idx, 1);  // remove it; we're done
+    }
+}
+
+function colorForTouch(touch) {
+    var r = touch.identifier % 16;
+    var g = Math.floor(touch.identifier / 3) % 16;
+    var b = Math.floor(touch.identifier / 7) % 16;
+    r = r.toString(16); // make it a hex digit
+    g = g.toString(16); // make it a hex digit
+    b = b.toString(16); // make it a hex digit
+    var color = "#" + r + g + b;
+    return color;
+}
+
+function copyTouch({ identifier, pageX, pageY }) {
+    return { identifier, pageX, pageY };
+}
+
+function ongoingTouchIndexById(idToFind) {
+    for (var i = 0; i < ongoingTouches.length; i++) {
+        var id = ongoingTouches[i].identifier;
+
+        if (id == idToFind) {
+            return i;
+        }
+    }
+    return -1;    // not found
+}
